36bae71fb9256b7dae92ad9a19ad49c810c3635a
benzo@cup.sh <benzo@cup.sh>
2023-10-19 03:41:08 +0300

    Update logic

--- main.py
+++ main.py
@@ -25,20 +25,41 @@ class InsertQuestions(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('questions_num', help='This field is required', required=True)
         args = parser.parse_args()
-        list_questions = get_questions(args['questions_num'])
+        list_questions_api = get_questions(args['questions_num'])
         session = create_session()
 
-        all_questions = session.query(Question.id).all()
+        all_questions_db = session.query(Question.id).all()
+
+        list_questions_api.append({
+            "id": 2848,
+            "answer": "counterclockwise",
+            "question": "In the Northern Hemisphere, hurricane winds blow around the storm's eye in this direction",
+            "value": 300,
+            "airdate": "1986-02-06T20:00:00.000Z",
+            "created_at": "2022-12-30T18:38:46.514Z",
+            "updated_at": "2022-12-30T18:38:46.514Z",
+            "category_id": 457,
+            "game_id": 2263,
+            "invalid_count": None,
+            "category": {
+                "id": 457,
+                "title": "storms",
+                "created_at": "2022-12-30T18:38:46.447Z",
+                "updated_at": "2022-12-30T18:38:46.447Z",
+                "clues_count": 8
+            }
+        })
         latest_question = None
         list_to_add = []
-        for question_item in list_questions:
-            if (question_item['id'] not in all_questions):
+        for question_item in list_questions_api:
+            if ((question_item['id'],) not in all_questions_db):
                 new_question = Question(id=question_item['id'], question_text=question_item['question'],
                                         answer_text=question_item['answer'], created_at=question_item['created_at'])
                 list_to_add.append(new_question)
                 latest_question = question_item
 
         session.add_all(list_to_add)
+        session.commit()
         session.close()
         return {'success': True, 'latest_question': latest_question}, 200
 
@@ -49,4 +70,4 @@ api.add_resource(InsertQuestions, '/api/questions/')
 
 if __name__ == '__main__':
     global_init('postgresql://baeldung:baeldung@212.193.51.197/baeldung')
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('port', '3000'))
+    app.run(debug=True, host='127.0.0.1', port=os.environ.get('port', '3000'))
